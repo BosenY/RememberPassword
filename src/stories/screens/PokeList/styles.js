5620821a7e1f114e2a55f0faa4dd4ec5a12fa0b0
boseny <yxy43445@gmail.com>
2018-06-06 17:59:14 +0800

    Finish pokemon list page

--- src/stories/screens/PokeList/styles.js
+++ src/stories/screens/PokeList/styles.js
@@ -2,11 +2,179 @@ import { StyleSheet } from 'react-native'
 
 const styles: any = StyleSheet.create({
   container: {
-    backgroundColor: '#FBFAFA'
+    backgroundColor: '#ffffff'
   },
   pokeListImg: {
-    width: 20,
-    height: 20
+    width: 40,
+    height: 40
+  },
+  itemListName: {
+    flex: 1
+  },
+  /*草*/
+  Grass: {
+    borderColor: '#40C60A',
+    backgroundColor: '#77CC55',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*毒*/
+  Poison: {
+    borderColor: '#792F6A',
+    backgroundColor: '#AA5599',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*火*/
+  Fire: {
+    borderColor: '#BA1F00',
+    backgroundColor: '#FF4422',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*龙*/
+  Dragon: {
+    borderColor: '#31229D',
+    backgroundColor: '#7766EE',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*飞行*/
+  Flying: {
+    borderColor: '#3678FF',
+    backgroundColor: '#6699FF',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*水*/
+  Water: {
+    borderColor: '#0D6AC8',
+    backgroundColor: '#3399FF',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*虫*/
+  Bug: {
+    borderColor: '#849400',
+    backgroundColor: '#AABB22',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*正常*/
+  Normal: {
+    borderColor: '#8A8A7B',
+    backgroundColor: '#BBBBAA',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*暗(恶)*/
+  Dark: {
+    borderColor: '#442C21',
+    backgroundColor: '#775544',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*电*/
+  Electric: {
+    borderColor: '#BD8E00',
+    backgroundColor: '#FFCC33',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*冰*/
+  Ice: {
+    borderColor: '#13A8D9',
+    backgroundColor: '#77DDFF',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*超能力*/
+  Psychic: {
+    borderColor: '#D00053',
+    backgroundColor: '#FF5599',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*钢*/
+  Steel: {
+    borderColor: '#74747B',
+    backgroundColor: '#AAAABB',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*地面*/
+  Ground: {
+    borderColor: '#B59226',
+    backgroundColor: '#DDBB55',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*妖精*/
+  Fairy: {
+    borderColor: '#EC67EA',
+    backgroundColor: '#FFAAFF',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*格斗*/
+  Fighting: {
+    borderColor: '#912E1E',
+    backgroundColor: '#BB5544',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*岩石*/
+  Rock: {
+    borderColor: '#88762C',
+    backgroundColor: '#BBAA66',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  /*幽灵*/
+  Ghost: {
+    borderColor: '#42428E',
+    backgroundColor: '#6666BB',
+    padding: 5,
+    width: 80,
+    borderRadius: 50,
+    margin: 10
+  },
+  itemText: {
+    color: '#ffffff'
   }
 })
 export default styles
